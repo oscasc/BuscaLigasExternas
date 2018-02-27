3482906bb1cbe36229486a3eee4a91bd80f800c8
Oscar A. Salgado C <oscar.salgado@neixar.com>
2018-02-27 10:44:25 -0600

    Inicio de proyecto

---
+++ src/com/neixar/sct/buscaLigas/Files/ListFiles.java
@@ -0,0 +1,20 @@
+package com.neixar.sct.buscaLigas.Files;
+
+import java.io.File;
+import java.util.Collection;
+import java.util.ArrayList;
+
+public class ListFiles {
+	
+	private Collection<File> file = new ArrayList<File>();
+	
+	public Collection<File> getFiles(File file){
+		
+		if(file.isDirectory())
+			
+		
+		
+	}
+	
+
+}
